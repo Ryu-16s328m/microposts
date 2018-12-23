6668d775b9ebf3b0d57190b50ed64274be406a29
Ryuutarou.Yamamoto <schuxinzhedesu@gmail.com>
2018-12-23 05:20:02 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
